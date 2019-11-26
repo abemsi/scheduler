17470130145eba271f47f31b0801946951d081ef
Amy <amy.bemister@gmail.com>
2019-11-26 11:08:57 -0700

    Add Header to Appointment stories

--- stories/index.js
+++ stories/index.js
@@ -23,6 +23,8 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment";
 
+import Header from "components/Appointment/Header";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -144,4 +146,5 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />);
