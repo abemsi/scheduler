59b118c27e3253b9852790d12dade39b9e70cff6
Amy <amy.bemister@gmail.com>
2019-11-26 12:10:33 -0700

    Update the Empty component to display the onAdd props

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -8,8 +8,9 @@ export default function Empty(props) {
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
+        onClick={props.onAdd}
       />
     </main>
-  );
-  
+    );
+
 }
