55fbc69e74163aebbbc002d124029ca42dc17da5
Amy <amy.bemister@gmail.com>
2019-11-27 01:01:52 -0700

    Add fragment to Appointment component, import Header, Show, Empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import "components/Appointment/styles.scss";
 
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
 export default function Appointment(props) {
   
-    return (
-      <article className="appointment">
+  return (
+    <Fragment>
+      <Header time={props.time} />
+      { props.interview ? <Show student={props.student} interviewer={props.interviewer} /> : <Empty /> }
+    </Fragment>
+  );
 
-      </article>
-    );
- 
-  
-  
 }
