402a526612359fb3eef84f40f6c18c0c0c09c9bd
Amy <amy.bemister@gmail.com>
2019-11-27 17:32:06 -0700

    Create get AppointmentForDay function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,8 @@
+export function getAppointmentsForDay(state, day) {
+  let appointments = [];
+  const filteredDays = state.days.filter(currentDay => currentDay.name === day);
+  if (filteredDays.length > 0 && filteredDays[0].appointments.length > 0) {
+    appointments = filteredDays[0].appointments.map((appointmentId) => state.appointments[appointmentId]);
+  }
+  return appointments;
+}
