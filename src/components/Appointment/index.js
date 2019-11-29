64ffe566ce19baa5b7d7c7d1d91a7c7a0e93d638
Amy <amy.bemister@gmail.com>
2019-11-29 16:06:21 -0700

    Add save function with transitions when booking interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,18 +5,36 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Saving from "components/Appointment/Saving";
 import useVisualMode from "../../hooks/useVisualMode.js";
 import Form from "./Form.js";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING"
 
 export default function Appointment(props) {
+
+  const { id, bookInterview } = props;
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    if (id && bookInterview) {
+      const interview = {
+        student: name,
+        interviewer
+      };
+      transition(SAVING);
+      bookInterview(id, interview)
+      .then(() => transition(SHOW));
+
+    }
+    
+  }
   
   return (
     <Fragment>
@@ -32,9 +50,10 @@ export default function Appointment(props) {
           <Form
             interviewers={props.interviewers}
             onCancel={() => back()}
-            onSave={props.onSave}
+            onSave={save}
           />
         )}
+        {mode === SAVING && <Saving />}
     </Fragment>
   );
 
