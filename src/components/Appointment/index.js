7f76247a1c177b27c2dd0c374f56a0455f1cec2b
Amy <amy.bemister@gmail.com>
2019-11-29 12:29:27 -0700

    Import useVisualMode to conditionally render components and add transitions

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,13 +5,36 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "../../hooks/useVisualMode.js";
+import Form from "./Form.js";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   
   return (
     <Fragment>
       <Header time={props.time} />
-      { props.interview ? <Show student={props.student} interviewer={props.interviewer} /> : <Empty /> }
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (
+          <Show 
+            student={props.interview.student} 
+            interviewer={props.interview.interviewer} 
+          />
+        )}
+        {mode === CREATE && (
+          <Form
+            interviewers={props.interviewers}
+            onCancel={() => back()}
+            onSave={props.onSave}
+          />
+        )}
     </Fragment>
   );
 
