bb98e504f508e03ea077814a07158321738b910b
Amy <amy.bemister@gmail.com>
2019-12-02 10:29:50 -0700

    Add eslint-disable to ignore setHistory

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  // eslint-disable-next-line
   const [history, setHistory] = useState([initial]);
   
   function transition(mode, replace = false) {
