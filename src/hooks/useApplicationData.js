36a7cbbcfc1ac250d74fa830cf29526bed8f224a
Amy <amy.bemister@gmail.com>
2019-12-02 10:30:24 -0700

    Add reducer function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,72 +1,110 @@
-import { useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
 
-  const [state, setState] = useState({
-    day: "",
+  const SET_DAY = "SET_DAY";
+  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  const SET_INTERVIEW = "SET_INTERVIEW";
+  const SET_SPOTS = "SET_SPOTS";
+  
+  const [state, dispatch] = useReducer(reducer, {
+    day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
   });
 
-  const setDay = day => setState({ ...state, day});
-
-
-  function bookInterview(id, interview) {
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-
-    return axios.put(`/api/appointments/${id}`, appointment)
-    .then((res) => {
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-        setState({ ...state, appointments });
-      })
-    
+  function reducer(state, action) {
+    switch (action.type) {
+      case SET_DAY:
+        return {
+          ...state,
+          day: action.day
+        }
+      case SET_APPLICATION_DATA: 
+        return {
+          ...state,
+          days: action.days,
+          appointments: action.appointments,
+          interviewers: action.interviewers
+        }
+      case SET_INTERVIEW:
+        return {
+          ...state.appointments,
+          [action.id]: { ...action.interview }
+        }
+      case SET_SPOTS:
+        return {
+          ...state,
+          days: action.days
+        }
+      default:
+        throw new Error(
+          `Tried to reduce with unsupported action type: ${action.type}`
+        )
+    }
   }
+          
 
+  const setDay = day => dispatch({ type: SET_DAY, day});
 
-  function cancelInterview(id, interview) {
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-
-    return axios.delete(`/api/appointments/${id}`)
-    .then((res) => {
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-        setState({ ...state, appointments });
-    })
-
-  }
-  
-  
   useEffect(() => {
-
     const promise1 = axios.get(`/api/days`);
     const promise2 = axios.get(`/api/appointments`);
     const promise3 = axios.get(`/api/interviewers`);
 
     Promise.all([promise1, promise2, promise3])
       .then((all) => {
-      console.log(all)
-      setState(prev => ({ days: all[0].data, appointments: all[1].data, interviewers: all[2].data })); 
+        dispatch({ 
+          type: SET_APPLICATION_DATA,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data 
+        }); 
       })
       .catch((error) => {
         console.log(error.response);
       })
   }, [])
 
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const subtractSpots = () => {
+      
+    }
+    return axios.put(`/api/appointments/${id}`, appointment)
+    .then((res) => {
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      };
+        dispatch({ type: SET_INTERVIEW, appointments });
+        dispatch({ type: SET_SPOTS, appointments });
+      })
+  }
+
+  function cancelInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const addSpots = () => {
+      
+    }
+    return axios.delete(`/api/appointments/${id}`)
+    .then((res) => {
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      };
+        dispatch({ type: SET_INTERVIEW, appointments });
+        dispatch({ type: SET_SPOTS, appointments });
+    })
+  }
 
   return { state, setDay, bookInterview, cancelInterview };
 }
