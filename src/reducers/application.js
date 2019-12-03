202d916ab767c75e7706a60c8d2aa2c209775306
Amy <amy.bemister@gmail.com>
2019-12-03 11:58:49 -0700

    Create file for reducer function and export with constants

---
+++ src/reducers/application.js
@@ -0,0 +1,37 @@
+export const SET_DAY = "SET_DAY";
+export const SET_INTERVIEW = "SET_INTERVIEW";
+export const SET_SPOTS = "SET_SPOTS";
+export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+
+export default function reducer(state, action) {
+
+  switch (action.type) {
+    case SET_DAY:
+      return {
+        ...state,
+        day: action.day
+      }
+    case SET_APPLICATION_DATA: 
+      return {
+        ...state,
+        days: action.days,
+        appointments: action.appointments,
+        interviewers: action.interviewers
+      }
+    case SET_INTERVIEW:
+      return {
+        ...state,
+        appointments: action.appointments,
+        days: action.days
+      }
+    case SET_SPOTS:
+      return {
+        ...state,
+        days: action.days
+      }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      )
+  }
+}
