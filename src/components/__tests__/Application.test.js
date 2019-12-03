936ffd1fd6dd9665b5971b27ccedb4a71fb5ff41
Amy <amy.bemister@gmail.com>
2019-12-02 21:13:54 -0700

    Add test for editing an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -83,4 +83,36 @@ describe("Application", () => {
     expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
   });
 
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    const { container } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+        target: { value: "New name" }
+      });
+    
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "New name"));
+
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+      );
+      
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
+  });
+
 });
+
