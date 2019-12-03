543eb28ae62cfe7e51eb8eefb7ccfafcfbdf7efa
Amy <amy.bemister@gmail.com>
2019-12-03 11:58:24 -0700

    Move reducer function to another file and import

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,51 +1,20 @@
 import { useReducer, useEffect } from "react";
 import axios from "axios";
 
-export default function useApplicationData() {
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "reducers/application";
 
-  const SET_DAY = "SET_DAY";
-  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-  const SET_INTERVIEW = "SET_INTERVIEW";
-  const SET_SPOTS = "SET_SPOTS";
+export default function useApplicationData() {
   
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
-  });
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case SET_DAY:
-        return {
-          ...state,
-          day: action.day
-        }
-      case SET_APPLICATION_DATA: 
-        return {
-          ...state,
-          days: action.days,
-          appointments: action.appointments,
-          interviewers: action.interviewers
-        }
-      case SET_INTERVIEW:
-        return {
-          ...state,
-          appointments: action.appointments,
-          days: action.days
-        }
-      case SET_SPOTS:
-        return {
-          ...state,
-          days: action.days
-        }
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        )
-    }
-  }     
+  });   
 
   const setDay = day => dispatch({ type: SET_DAY, day});
 
